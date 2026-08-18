40f5a755eb4e4b9b6bf136d460f7ae7454958250
Viacheslav-Bo <zotikivna@gmail.com>
2026-08-19 00:51:16 +0300

    feat(auth): register refresh/logout/me routes

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,12 +1,19 @@
 import { Router } from "express";
 import { signupController } from "../controllers/authControllers/signupController.js";
 import { loginController } from "../controllers/authControllers/loginController.js";
+import { logoutController } from "../controllers/authControllers/logoutController.js";
 import { validateBody } from "../middlewares/validateBody.js";
 import { signupSchema, loginSchema } from "../validation/authSchemas.js";
+import { authenticate } from "../middlewares/authenticate.js";
+import { refreshController } from "../controllers/authControllers/refreshController.js";
+import { getUserInfoController } from "../controllers/authControllers/getUserInfoController.js";
 
 const router = Router();
 
-router.post("/signup", validateBody(signupSchema), signupController);
-router.post("/login", validateBody(loginSchema), loginController);
+router.get("/user/user-info", authenticate, getUserInfoController);
+router.post("/user/signup", validateBody(signupSchema), signupController);
+router.post("/user/login", validateBody(loginSchema), loginController);
+router.post("/user/logout", authenticate, logoutController);
+router.post("/user/refresh", refreshController);
 
 export default router;
