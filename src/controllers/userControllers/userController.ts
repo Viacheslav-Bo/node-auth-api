f3d088b21ecfb9cf5290413fb50464baa10aa4c5
Viacheslav-Bo <zotikivna@gmail.com>
2026-08-20 16:30:55 +0300

    fix way for userController

--- src/controllers/userControllers/userController.ts
+++ src/controllers/userControllers/userController.ts
@@ -1,5 +1,5 @@
 import type { Request, Response } from "express";
-import * as userService from "../../services/userServices";
+import * as userService from "../../services/userServices/userService.js";
 import createHttpError from "http-errors";
 
 export const getAllUsers = async (req: Request, res: Response) => {
